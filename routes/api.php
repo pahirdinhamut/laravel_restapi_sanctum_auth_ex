fa6509ac68b900f9f511ec9795cbf206932a4a30
pahirdinhamut <fahreddin803@gmail.com>
2021-04-13 15:41:43 +0300

    Api Crud

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,17 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
+// Route::get('/products', [ProductController::class, 'index']);
+
+
+Route::resource('products', ProductController::class);
+Route::get('/products/search/{carname}', [ProductController::class, 'search']);
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
