7217944bd93a500776d3dcb3b89333dd409dfece
pahirdinhamut <fahreddin803@gmail.com>
2021-04-13 16:34:01 +0300

    Auth Register

--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,25 @@
 <?php
 
-use App\Http\Controllers\ProductController;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
 
-// Route::get('/products', [ProductController::class, 'index']);
-
+//public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
 
 Route::resource('products', ProductController::class);
-Route::get('/products/search/{carname}', [ProductController::class, 'search']);
 
+// Protected routes 
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
